901dbc465cb89a76b6285cdcdf239589a89dee41
Jack Nelson <jack_nelson@berkeley.edu>
2026-02-20 14:48:05 -0800

    Fix loop to iterate based on second argument

--- test.py
+++ test.py
@@ -4,8 +4,6 @@ import numpy as np
 import sys
 
 arguments = sys.argv
-print(arguments[0])
-print(len(arguments[0]))
 
 file = open("test.csv", "w", newline = None)
 
@@ -14,7 +12,7 @@ csvwriter = csv.writer(file, delimiter=',')
 meta = ['time', 'data']
 csvwriter.writerow(meta)
 
-for i in range(int(arguments[0])):
+for i in range(int(arguments[1])):
     now = time.time()
     value = np.random.random()
     csvwriter.writerow([now, value])
